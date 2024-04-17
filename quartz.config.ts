e8b4514e0462d4e0e17a916ae732a91dcee046d4
mitchellpound <mitchell.pound@usu.edu>
2024-04-17 00:40:17 -0600

    Quartz sync: Apr 17, 2024, 12:40 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "Braindump/Templates", ".obsidian"],
+    ignorePatterns: ["private", "Templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
